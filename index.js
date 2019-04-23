86383982ff3d58e2753f159d73f45a7e8fb8c74c
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 22:20:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(ob, key, val) {
   return Object.assign({}, ob, {[key]: val});
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(ob, key, val) {
+  ob[key] = val;
+  return ob;
+}
+
